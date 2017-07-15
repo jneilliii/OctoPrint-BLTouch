2e42b562db661b2942b6f4608489a021ccc84f0c
jneilliii <jneilliii+github@gmail.com>
2017-07-15 01:50:56 -0400

    Added settings template to control GCODE commands used for the different actions.  Defaults to Marlin firmware commands.

--- octoprint_BLTouch/static/js/BLTouch.js
+++ octoprint_BLTouch/static/js/BLTouch.js
@@ -7,6 +7,12 @@
 $(function() {
     function BLTouchViewModel(parameters) {
         var self = this;
+		self.settingsViewModel = parameters[1];
+		
+		self.cmdProbeUp = ko.observable();
+		self.cmdProbeDown = ko.observable();
+		self.cmdSelfTest = ko.observable();
+		self.cmdReleaseAlarm = ko.observable();
 
         // assign the injected parameters, e.g.:
 		self.controlViewModel = parameters[0];
@@ -14,13 +20,27 @@ $(function() {
 		self.getAdditionalControls = function() {
             return [
                 { name: "BLTouch", type: "section", layout: "horizontal", children: [
-                    {type: "command", command: "M280 P0 S90", name: "Probe Up"},
-                    {type: "command", command: "M280 P0 S10", name: "Probe Down"},
-                    {type: "command", command: "M280 P0 S120", name: "Self Test", confirm: "Are you sure you want to run a self test?"},
-                    {type: "command", command: "M280 P0 S160", name: "Release Alarm", confirm: "Are you sure you want to release the alarm?"},
+                    {type: "command", command: self.cmdProbeUp, name: "Probe Up"},
+                    {type: "command", command: self.cmdProbeDown, name: "Probe Down"},
+                    {type: "command", command: self.cmdSelfTest, name: "Self Test", confirm: "Are you sure you want to run a self test?"},
+                    {type: "command", command: self.cmdReleaseAlarm, name: "Release Alarm", confirm: "Are you sure you want to release the alarm?"},
                 ]}
             ];
         };
+		
+		self.onBeforeBinding = function() {
+            self.cmdProbeUp(self.settingsViewModel.settings.plugins.BLTouch.cmdProbeUp());
+            self.cmdProbeDown(self.settingsViewModel.settings.plugins.BLTouch.cmdProbeDown());
+            self.cmdSelfTest(self.settingsViewModel.settings.plugins.BLTouch.cmdSelfTest());
+            self.cmdReleaseAlarm(self.settingsViewModel.settings.plugins.BLTouch.cmdReleaseAlarm());
+        }
+		
+		self.onEventSettingsUpdated = function (payload) {            
+            self.cmdProbeUp = self.settingsViewModel.settings.plugins.BLTouch.cmdProbeUp();
+            self.cmdProbeDown = self.settingsViewModel.settings.plugins.BLTouch.cmdProbeDown();
+            self.cmdSelfTest = self.settingsViewModel.settings.plugins.BLTouch.cmdSelfTest();
+            self.cmdReleaseAlarm = self.settingsViewModel.settings.plugins.BLTouch.cmdReleaseAlarm();
+        }
     }
 
     // view model class, parameters for constructor, container to bind to
@@ -28,9 +48,9 @@ $(function() {
         BLTouchViewModel,
 
         // e.g. loginStateViewModel, settingsViewModel, ...
-        ["controlViewModel"],
+        ["controlViewModel,settingsViewModel"],
 
         // e.g. #settings_plugin_BLTouch, #tab_plugin_BLTouch, ...
-        [ /* ... */ ]
+        ["settings_plugin_BLTouch_form"]
     ]);
 });
