31303d36864774cc000151a3589c4daca2928b8e
jneilliii <jneilliii+github@gmail.com>
2017-07-15 10:10:21 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,10 +11,10 @@ plugin_package = "octoprint_BLTouch"
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
-plugin_name = "OctoPrint-Bltouch"
+plugin_name = "OctoPrint-BLTouch"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "0.1.0"
+plugin_version = "0.2.0"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
